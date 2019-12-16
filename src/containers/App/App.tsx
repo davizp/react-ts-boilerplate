ac9ce6669dc587abcb25566429efd378cceeb673
David Nuñez <dh_nz@hotmail.com>
2019-12-16 14:29:04 -0600

    Added React Flexbox Grid.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import classes from './App.module.scss';
-import MainRoutes from './AppRoutes';
+import AppRoutes from './AppRoutes';
 import Menu from './components/Menu/Menu';
 
 const App: React.FC = () => {
@@ -9,7 +9,7 @@ const App: React.FC = () => {
     <div className={classes.app}>
       <Header />
       <Menu />
-      <MainRoutes />
+      <AppRoutes />
     </div>
   );
 };
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import classes from './Home.module.scss';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const Home: React.FC = () => {
   return (
-    <div className={classes.home}>
-      <h2>Home Page!</h2>
-    </div>
+    <Grid className={classes.home} fluid>
+      <Row>
+        <Col xs={6} md={12}>
+          <h2>Home Page!</h2>
+        </Col>
+
+        <Col xs={12} md={4}>
+          Column 1!
+        </Col>
+
+        <Col xs={12} md={4}>
+          Column 2!
+        </Col>
+
+        <Col xs={12} md={4}>
+          Column 3!
+        </Col>
+      </Row>
+    </Grid>
   );
 };
 
