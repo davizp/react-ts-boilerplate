0d749a848a41af2614310240a924809cf5e5ad6f
David Nuñez <dh_nz@hotmail.com>
2019-12-15 22:53:31 -0600

    Fixed redux dev tools error.

--- src/store/setupStore.ts
+++ src/store/setupStore.ts
@@ -4,27 +4,23 @@ import rootReducer from './reducers/';
 import rootSaga from './sagas';
 
 function setupStore(initialState = {}) {
-
   // Middleware
   const sagaMiddleware = createSagaMiddleware();
 
-  const middleware = applyMiddleware(...[
-    sagaMiddleware,
-  ]);
+  const middleware = applyMiddleware(...[sagaMiddleware]);
 
   const isDevelopment = process.env.NODE_ENV !== 'production';
 
-  const reduxDevTools = isDevelopment && (
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  );
+  const composeEnhancers = isDevelopment
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
+  const realMiddleware = isDevelopment
+    ? composeEnhancers(middleware)
+    : middleware;
 
   // Store
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(middleware, reduxDevTools)
-  );
+  const store = createStore(rootReducer, initialState, realMiddleware);
 
   // Sagas
   sagaMiddleware.run(rootSaga);
